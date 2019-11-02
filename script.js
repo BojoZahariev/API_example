cd1bec97b9d469f6ee1afac41f2c1c09c1c5df31
Bojo <bozhidar_zahariev@hotmail.com>
2019-11-02 14:22:20 +0000

    button

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const img = document.querySelector('img');
+const img = document.getElementById('img');
+const buttonNewGif = document.getElementById('newGif');
 let keyWord = 'dog';
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=B2sknp7nYaRlopzefXO01xnnlFymv0zN&s=' + keyWord, { mode: 'cors' })
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    img.src = response.data.images.original.url;
-  })
 
-  .catch(err => alert(err));
+const giffys = () => {
+  fetch('https://api.giphy.com/v1/gifs/translate?api_key=B2sknp7nYaRlopzefXO01xnnlFymv0zN&s=' + keyWord, {
+    mode: 'cors'
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      img.src = response.data.images.original.url;
+    })
+
+    .catch(err => alert(err));
+};
+
+giffys();
+
+buttonNewGif.addEventListener('click', giffys);
